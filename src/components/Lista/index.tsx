4f545f4984acbfc9f03b7b416b1488893f315162
Joyci Santos <joycisabrina@gmail.com>
2024-09-18 20:32:39 -0300

    first commit

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import style from './lista.module.scss'
+import Item from "./Item";
+
+function Lista() {
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    },
+    {
+        tarefa: 'Javascript',
+        tempo: '01:00:00'
+    }]
+    return (
+        <aside className={style.listaTarefas}>
+            <h2>Estudo do dia</h2>
+
+            <ul>
+                {tarefas.map((item, index) => (
+                   <Item 
+                   key={index}
+                    {...item}
+                   />
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
