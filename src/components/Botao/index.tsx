ca3cfaec93a641c12796efde9f83ae167ca01819
Joyci Santos <joycisabrina@gmail.com>
2024-09-19 19:11:09 -0300

    Finalização da aplicação

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import style from './botao.module.scss'
 
-class Botao extends React.Component<{ texto: string }>{
-    render(){
-        return(
-            <button className={style.botao} >
-                {this.props.texto}
-            </button>
-        )
-    }
+interface Props{
+    type?: "button" | "submit" | "reset" | undefined, 
+    onClick?: () => void,
+    children?: React.ReactNode
+}
+
+function Botao({onClick, type, children}: Props) {
+    return (
+        <button onClick={onClick} type={type} className={style.botao} >
+            {children}
+        </button>
+    )
 }
 
 export default Botao;
